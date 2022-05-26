e098f64c944ed52b8e33d58c38f5e62464518cc1
Thomas <thomassidlarski@outlook.de>
2022-05-26 20:55:48 +0200

    home seite hinzugefügt

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("weather.html")
+    return render_template("home.html")
 
 
-@app.route("/weather", methods = ["POST"])
+@app.route("/weather", methods = ["GET", "POST"])
 def weather():
+    if request.method == "GET": 
+        return render_template("weather.html")
     stadt = request.form["Stadt"]
     city,weather_description,temperature = weather_api.getLocation(stadt)
     print(city,weather_description,temperature)
